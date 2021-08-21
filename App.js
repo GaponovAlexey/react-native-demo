eb614f63a6fea029ffc5c3929fefc20f9e86b3e4
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-08-21 16:23:57 +0300

    todo

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AddTodo } from './src/AddTodo';
 import { Navbar } from './src/Navbar';
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: title
+    }
+
+    //setTodos(todos.concat([newTodo]))
+    //setTodos((prevTodos) => {
+    //  return [
+    //    ...prevTodos,
+    //    newTodo
+    //  ]
+    //})
+    setTodos(prev => [...prev, {
+      id: Date.now().toString(),
+      title
+    }])
+  }
   return (
-    <View style={ styles.container }>
-      <Navbar title={'Title-App!'}/>
+    <View >
+      <Navbar title={ 'Title-Appsrr!' } />
+      <View style={ styles.container } >
+        <Text>last name</Text>
+        <AddTodo onSubmit={ addTodo } />
+        <View>
+          { todos.map(todo => <Todo key={ todo.id } todo={ todo.title } />
+          ) }
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +43,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
-  text: {
-  }
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import { StyleSheet, TextInput, View, Button } from 'react-native'
 
-const AddTodo = () => {
+export const AddTodo = ({ onSubmit }) => {
+	const pressHandler = () => {
+		onSubmit('test todo')
+	}
+
 	return (
 		<View style={ styles.block }>
-			<TextInput ></TextInput>
-			<Button title={} />
+			<TextInput style={ styles.input } />
+			<Button onPress={ pressHandler } title='Добавить' />
 		</View>
 	)
 }
 
 
 
-export default AddTodo
+
 
 const styles = StyleSheet.create({
-	blocl: {
-
+	block: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	input: {
+		width: '70%',
+		padding: 10,
+		borderStyle: 'solid',
+		borderBottomWidth: 2,
+		borderBottomColor: '#3949ab',
 	}
 })
